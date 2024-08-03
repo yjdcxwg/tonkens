89f7fed90b57c43b34b4bd292537221238668bea
yjdcxwg <yjdcxwg@hotmail.com>
2024-08-03 08:49:59 +0800

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,40 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/manager/AccessManaged.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract MyToken is ERC721, ERC721Pausable, AccessManaged, ERC721Burnable {
+    uint256 private _nextTokenId;
+
+    constructor(address initialAuthority)
+        ERC721("MyToken", "MTK")
+        AccessManaged(initialAuthority)
+    {}
+
+    function pause() public restricted {
+        _pause();
+    }
+
+    function unpause() public restricted {
+        _unpause();
+    }
+
+    function safeMint(address to) public restricted {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+}
